7db8734ae18f17d7a60f7092f26348d5e65ba586
Dhimas Bagus Prayoga <dhimasbagusprayoga@gmail.com>
2019-12-23 09:27:53 +0700

    Refactor to krypton

---
+++ userbot/modules/userbug.py
@@ -0,0 +1,72 @@
+# Copyright (C) 2019 The Raphielscape Company LLC.
+#
+# Licensed under the Raphielscape Public License, Version 1.c (the "License");
+# you may not use this file except in compliance with the License.
+#
+# You can find misc modules, which dont fit in anything xD
+""" Userbot module for other small commands. """
+
+from random import randint
+from asyncio import sleep
+from os import execl
+import sys
+import os
+import io
+import sys
+import json
+from userbot import BOTLOG, BOTLOG_CHATID, CMD_HELP, bot
+from userbot.events import register
+
+@register(outgoing=True, pattern="^!krypton$")
+async def shalom(e):
+    await e.edit(
+        "\n _  __"
+        "\n| |/ /"
+        "\n| ' /"
+        "\n| . \"
+        "\n|_|\_\"
+        "\n"
+        "\n _ __"
+        "\n| '__|"
+        "\n| |"
+        "\n|_|"
+        "\n"
+        "\n _   _"
+        "\n| | | |"
+        "\n| |_| |"
+        "\n \__, |"
+        "\n |___/"
+        "\n"
+        "\n ____"
+        "\n|  _ \"
+        "\n| |_) |"
+        "\n|  __/"
+        "\n|_|"
+        "\n"
+        "\n _"
+        "\n| |_"
+        "\n| __|"
+        "\n| |_"
+        "\n \__|"
+        "\n"
+        "\n  ___"
+        "\n / _ \"
+        "\n| (_) |"
+        "\n \___/"
+        "\n"
+        "\n _   _"
+        "\n| \ | |"
+        "\n|  \| |"
+        "\n| |\  |"
+        "\n|_| \_|")
+        
+    
+    CMD_HELP.update({
+    'krypton':
+    '.krypton\
+\nUsage: gives a nice KryPtoN as output😂.'
+})
+
+
+
+
